fe6c449dd1824e68bcaac1f4602bdda6efd868c3
montsechen <jchen35@iesnumancia.cat>
2017-01-23 13:23:11 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,1 +1,49 @@
+$(document).on('deviceready', function() {
+
+     // determinem amplada alçada DISPOSITIU ------------------
+     var amplada_pantalla = screen.width ;		// 720px  --> SG_Note2
+     var alcada_pantalla = screen.height ; 		// 1280px
+     var amplada_pantalla_CSS = window.innerWidth ; 	// 360px
+     var alcada_pantalla_CSS = window.innerHeight ;	// 616px 
+     /////////////////////////////////////////////////////////
+     
+     // REDIMENSIONEM EL CANVAS
+     var canvas = document.getElementById('canvas');
+     var ctx = canvas.getContext('2d');
+     ctx.canvas.width  = window.innerWidth;
+     ctx.canvas.height = window.innerHeight;
+     
+	// centre pantalla ?
+	var centre_x = amplada_pantalla_CSS / 2 ;
+	var centre_y = alcada_pantalla_CSS / 2 ;  // var centre_y = window.innerHeight / 2 ; //
+		
+	// Quina posició la bola ? Temin present que la bola ocupa un espai 	// quina mida la bola ?
+	var mida_x_bola = amplada_pantalla_CSS * ( 10 / 100 ) ; 
+	var mida_y_bola = mida_x_bola ;  // 36 ;
+	var posicio_x_bola = centre_x - ( mida_x_bola / 2 ) ;
+	var posicio_y_bola = centre_y - ( mida_y_bola / 2 ) ;
+ 	
+ 	
+	document.addEventListener("offline", function() { 
+		 alert("ara NO HI HA internet");
+	}, false);
+ 
+	$(window).resize(function() {
+		alert("has girat el dispositiu");
+	}, false); 
+	
+	var nivell = 1 ;
+	
+	 ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 50, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect (30, 30, 50, 50);
+	
+	
+});	
+	
+
+
+
 
